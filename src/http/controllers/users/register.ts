dba9da89dcc333ba6799c70fd4fc84d2ee36d338
SENIOR\joao.souza <joao.souza@senior.com.br>
2025-01-26 19:01:24 -0300

    Implementado controlers e testes automatizados

--- src/http/controllers/users/register.ts
+++ src/http/controllers/users/register.ts
@@ -1,4 +1,4 @@
-import { FastifyRequest, FastifyReply } from 'fastify'
+import { FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod'
 import { UserAlreadyExistsError } from '@/services/errors/user-already-exists-error'
 import { makeRegisterService } from '@/services/factories/make-register-service'
@@ -13,15 +13,19 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
   const { name, email, password } = registerBodySchema.parse(request.body)
 
   try {
-    const registerService = makeRegisterService()
+    const registerUseCase = makeRegisterService()
 
-    await registerService.execute({ name, email, password })
-  } catch (error) {
-    if (error instanceof UserAlreadyExistsError) {
-      return reply.status(409).send({ message: error.message })
+    await registerUseCase.execute({
+      name,
+      email,
+      password,
+    })
+  } catch (err) {
+    if (err instanceof UserAlreadyExistsError) {
+      return reply.status(409).send({ message: err.message })
     }
 
-    throw error
+    throw err
   }
 
   return reply.status(201).send()
